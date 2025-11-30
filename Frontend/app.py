792587d17aaaea17bad151610b70a9c754931c62
volequyen <qvo9110@gmail.com>
2025-11-30 20:00:24 +0700

    fix FE

--- Frontend/app.py
+++ Frontend/app.py
@@ -7,5 +7,6 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    # Port 5000 cho frontend
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
